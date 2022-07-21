b311ba27d86d52aee9597aeb23733cc9ba2db89a
Amira <amiragucher@gmail.com>
2022-07-21 19:28:38 +0100

    About me CV text added to AboutMeText

--- portfolio/Components/AboutMe/AboutMeText.js
+++ portfolio/Components/AboutMe/AboutMeText.js
@@ -3,23 +3,8 @@ import Style from "./AboutMe.module.css"
 export default function AboutMe (){
     return (
         <div>
-        <h2 className={Style.AboutMeText}>lor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore 
-            et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut 
-            aliquip ex ea commodo consequat. Duis aute irure 
-            dolor in reprehenderit in voluptate velit esse cillum 
-            dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-            cupidatat non proident, sunt in culpa qui officia deserunt 
-            mollit anim id est laborum.lor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore 
-            et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut 
-            aliquip ex ea commodo consequat. Duis aute irure 
-            dolor in reprehenderit in voluptate velit esse cillum 
-            dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-            cupidatat non proident, sunt in culpa qui officia deserunt 
-            mollit anim id est laborum.</h2>
+        <h2 className={Style.AboutMeText}>I am a hardworking, reliable, conscientious and approachable individual. I am currently seeking full-time employment and I am available to work immediately. I have worked in a variety of positions including customer service, waitressing and care work. Through my employment and volunteering experience, I have gained experience of working as a team as well as independently. I have a strong drive to learn new skills and am a fast learner. 
+</h2>
     </div>
 )
 
