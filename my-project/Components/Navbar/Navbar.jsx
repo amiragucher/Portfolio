d1a42c9178568e37da1ea80a838a8b7521f40e47
amiragucher <amiragucher@gmail.com>
2022-08-20 16:01:53 +0100

    Basic navbar created

--- my-project/Components/Navbar/Navbar.jsx
+++ my-project/Components/Navbar/Navbar.jsx
@@ -1,47 +1,32 @@
 import Link from "next/link";
 
-export default function Navbar (){
-  return (
-    //My Logo
-  
-    <div className="fixed z-[1002]">
-      <div className=" flex justify-between w-[100vw] bg-[#004F54] text-[white] ml-[0px] px-[1em] py-[1em]">
-        <div className="h-10 w-10 sm:h-14 sm:w-14 relative">
-          <Image
-            src="/logo-app.png"
-            alt="Project Clean-up logo"
-            layout="fill"
-            objectFit="cover"
-          />
-          
+export default function Navbar(){
+    return (
+        <div className="fixed z-[1002] flex-row">
+        <div className=" flex justify-between  w-[100vw] bg-[#004F54] text-[white] ml-[0px] px-[1em] py-[1em]">
+          <div className=" flex-row h-10 w-10 sm:h-14 sm:w-14 relative ">
+            <Link href="/#home">
+            <a className="text-[white] hidden md:flex items-center text-xl">
+              About Me
+            </a>
+          </Link>
+          <Link href="/#home">
+            <a className="text-[white] hidden md:flex items-center text-xl">
+              About Me
+            </a>
+          </Link>
+          <Link href="/#home">
+            <a className="text-[white] hidden md:flex items-center text-xl">
+              About Me
+            </a>
+          </Link>
+          <Link href="/#home">
+            <a className="text-[white] hidden md:flex items-center text-xl">
+              About Me
+            </a>
+          </Link>
         </div>
-        <Link href="#home">
-          <a className="text-[white] hidden md:flex items-center text-xl">
-            Home
-          </a>
-        </Link>
-       
-        <Link href="#home">
-          <a className="text-[white] hidden md:flex items-center text-xl">
-            Home
-          </a>
-        </Link>
-
-        <Link href="#home">
-          <a className="text-[white] hidden md:flex items-center text-xl">
-            Home
-          </a>
-        </Link>
-
-        <Link href="#home">
-          <a className="text-[white] hidden md:flex items-center text-xl">
-            Home
-          </a>
-        </Link>
-      </div>
-      </div>
-    
-       
-  );
-};
-
+        </div>
+        </div>
+    )
+}
