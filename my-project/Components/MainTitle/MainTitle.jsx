18955d3032b4c2713f50391733ccdac84f04704d
amiragucher <amiragucher@gmail.com>
2022-08-21 00:52:25 +0100

    final changes for today made

--- my-project/Components/MainTitle/MainTitle.jsx
+++ my-project/Components/MainTitle/MainTitle.jsx
@@ -4,7 +4,7 @@ export default function MainTitle() {
             <div>
                 <div className="flex-col justify-center text-xl  mt-[10em] ml-[1em] w-[100%] uppercase">
                     <h1 className="text-7xl drop-shadow-2xl text-[#E49315]">Amira Gucher-Blackman</h1>
-                    <h2 className="text-5xl drop-shadow-2xl text-[#a83028]">Full-Stack Developer</h2>
+                    <h2 className="text-5xl drop-shadow-8xl text-[#a83028]">Full-Stack Developer</h2>
                 </div>
                 <div className="flex justify-center items-center">
                 <img src="./QRCode.png" className="flex items-center mt-[5em]" />
