12eebd9d3a1f011f554c6a34bf05c16ec91da505
amiragucher <amiragucher@gmail.com>
2022-08-24 01:30:38 +0100

    Updating SVG name

--- my-project/Components/ToolBox/ToolBox.jsx
+++ my-project/Components/ToolBox/ToolBox.jsx
@@ -1,7 +1,7 @@
 export default function Toolbox (){
     return (
     <div className="flex justify-center items-center">
-        <img src="toolbox.svg" className="flex justify-center items-center"/>
+        <img src="Toolbox.svg" className="flex justify-center items-center"/>
     </div>
 
     )
