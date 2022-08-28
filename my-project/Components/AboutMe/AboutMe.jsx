8821f5438c10052a060a0b2b23285cb2b9541f75
amiragucher <amiragucher@gmail.com>
2022-08-28 01:24:30 +0100

    about me title changed to white

--- my-project/Components/AboutMe/AboutMe.jsx
+++ my-project/Components/AboutMe/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe (){
     return (
         <div className="flex-col bg-red-700 drop-shadow-lg text-white font-bold mt-0 p-5 h-full  w-full">
             <div>
-            <h1 className="text-5xl">About Me</h1>
+            <h1 className="text-5xl  sm:text-7xl w-[100%] drop-shadow-2xl text-white">About Me</h1>
             </div>
             <p id="aboutme">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
