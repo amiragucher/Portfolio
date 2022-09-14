78180f77c830b42216b38d4f019329693f902697
amiragucher <amiragucher@gmail.com>
2022-09-14 02:19:39 +0100

    All images of projects added to the cards, text added need to re-read

--- my-project/Components/AboutMe/AboutMe.jsx
+++ my-project/Components/AboutMe/AboutMe.jsx
@@ -4,11 +4,16 @@ export default function AboutMe (){
             <div>
             <h1 className="text-5xl  sm:text-7xl w-[100%] drop-shadow-2xl text-white">About Me</h1>
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <p>Prior to studying at the School of Code my studies evolved around
+                Art and Design, and then Psychology. During the bootcamp I found
+                a true passion for coding which has only grown over time. I have
+                experience of working with people from all walks of life and have
+                gained the ability to work within a team and in an agile framework.
+                Through the School of Code, I have developed both leadership and
+                team work skills and the ability to problem solve and come up with
+                solutions. I am committed, hard working, pro-active and passionate
+                and am ready to contribute to a companies successes.
+
             </p>
         </div>
     )
--- my-project/Components/Projects/Projects.jsx
+++ my-project/Components/Projects/Projects.jsx
@@ -16,19 +16,18 @@ export default function (){
                 </h2>
             </div>
             <div className="bg-white rounded-md m-2 p-1">
-                <h1 className="flex-col text-center text-xl">Project Clean-Up</h1>
+                <h1 className="flex-col text-center text-xl">To-Do List App</h1>
                 <img src="./ToDoList.png"/>
-                <h2>This project was my teams final app for our four week project at the School of Code. 
-                    We created this app to provide individuals with a platform that enables them to get 
-                    involved with cleaning up their local area. 
+                <h2>During a hackathon I learn how to created a to do list in react. Following the hackathon I decided to 
+                    re-create the to do list app and created this app which allows the user to add to the list, edit the list
+                    and delete from the list. 
                 </h2>
             </div>
             <div className="bg-white rounded-md m-2 p-1">
-                <h1 className="flex-col text-center text-xl">Project Clean-Up</h1>
-                <img src="./ProjectCleanup.png"/>
-                <h2>This project was my teams final app for our four week project at the School of Code. 
-                    We created this app to provide individuals with a platform that enables them to get 
-                    involved with cleaning up their local area. 
+                <h1 className="flex-col text-center text-xl">Global News App</h1>
+                <img src="./NewsApp.png"/>
+                <h2>I created this app in react as a side project and used a global news api. Whilst creating this app
+                    I was able to improve my css skills and I became more familiar with how to use react.
                 </h2>
             </div>
         </div>
